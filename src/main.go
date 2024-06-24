4f7ca8af0bb02220d7ddf81f723efa7cb6d81049
Lou Onezime <lou.onezime@epitech.eu>
2024-06-24 17:24:40 +0200

    feat(match): test parse arg to proceed with match option

--- src/main.go
+++ src/main.go
@@ -1,9 +1,30 @@
 package main
 
 import (
+	"flag"
+	"log"
 	"fmt"
+	// "BruteForce/src/matcher"
+)
+
+var (
+    WarningLogger *log.Logger
+    InfoLogger    *log.Logger
+    ErrorLogger   *log.Logger
 )
 
 func main() {
-	fmt.Println("Hello World")
+	matchPtr := flag.String("match", "", "a string")
+	usagePtr := flag.Bool("help", false, "a bool")
+
+	flag.Parse()
+	if *usagePtr {
+		fmt.Println("Usage: use flag match=<option>")
+		return;
+	}
+	if *matchPtr != "" {
+		log.Println("match:", *matchPtr)
+		return;
+	}
+	log.Println("No match indicated.")
 }
