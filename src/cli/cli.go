2f5f902c743daee698f08d28721b7cea61cc66bb
SIMLUKE <luc.simon@epitech.eu>
2024-07-23 14:39:08 +0200

    feat: autocompletion for cli (zsh-bash)

--- src/cli/cli.go
+++ src/cli/cli.go
@@ -8,14 +8,19 @@ import (
 
 func Parse_cli_args() (models.Forcing_params, error) {
 	var params models.Forcing_params
-	myError := errors.New("Wrong parameters")
+	UrlError := errors.New("No url given")
+	ThreadsError := errors.New("Wrong number of threads given")
+
 	forkptr := flag.Bool("v", false, "Verbose program")
 
 	flag.IntVar(&params.Threads, "threads", 1, "Number of threads to be used")
 	flag.Parse()
 
+	if params.Threads < 1 {
+		return params, ThreadsError
+	}
 	if len(flag.Args()) != 1 {
-		return params, myError
+		return params, UrlError
 	}
 	params.Url = flag.Args()[0]
 	params.BoolFlags.Verbose = *forkptr
