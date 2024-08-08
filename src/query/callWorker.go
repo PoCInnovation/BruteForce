d967998e22b3011b5feb77a1cbf3eed9f7588261
57ave <gustave.delecroix@epitech.eu>
2024-08-08 18:04:24 +0200

    feat():stop worker

--- src/query/callWorker.go
+++ src/query/callWorker.go
@@ -3,7 +3,7 @@ package query
 import (
 	"bruteforce/src/matching"
 	"bruteforce/src/utils"
-	"time"
+	"sync"
 )
 
 type ForceData struct {
@@ -12,22 +12,25 @@ type ForceData struct {
 	Url      string
 }
 
-func executeQueryFromFile(data ForceData, currentPath chan string) {
+func executeQueryFromFile(wg *sync.WaitGroup, data ForceData, currentPath chan string) {
+	defer wg.Done()
 	for taskData := range currentPath {
 		QueryExecute(data, taskData, "POST")
 	}
 }
 
 func MainRequest(data ForceData, criteria matcher.MatchCriteria) {
+	wg := &sync.WaitGroup{}
+    wg.Add(data.Worker)
 	channel := make(chan string)
 	wordArray := utils.GetFileContent(data.WordList)
 
 	for i := 0; i < data.Worker; i++ {
-		go executeQueryFromFile(data, channel)
+		go executeQueryFromFile(wg, data, channel)
 	}
 	for i := 0; i < len(wordArray); i++ {
 		channel <- wordArray[i]
 	}
-	time.Sleep(1 * time.Second)
+	wg.Wait()
 	close(channel)
 }
